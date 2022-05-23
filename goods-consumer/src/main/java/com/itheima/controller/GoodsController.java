9d7ec3c760c493a336b25a1a60b3930e7e9c32cd
高贵云 <1565646447@qq.com>
2022-05-23 14:44:16 +0800

    应该能用的初始版

--- goods-consumer/src/main/java/com/itheima/controller/GoodsController.java
+++ goods-consumer/src/main/java/com/itheima/controller/GoodsController.java
@@ -3,6 +3,7 @@ package com.itheima.controller;
 /**
  * Created by itcast on 2019/10/31.
  */
+
 import com.itheima.b2b.commonmodule.model.Cart;
 import com.itheima.b2b.commonmodule.model.Goods;
 import com.itheima.service.GoodsService;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
